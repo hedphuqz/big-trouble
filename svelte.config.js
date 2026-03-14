915797c80b55e02be5834aac74189bc850fd5f2e
Francis Powlesland <francis.powlesland1@ibm.com>
2026-03-14 06:26:41 +0100

    ci: add GitHub Pages deployment workflow

--- svelte.config.js
+++ svelte.config.js
@@ -2,7 +2,18 @@ import adapter from '@sveltejs/adapter-static';
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: { adapter: adapter() },
+	kit: {
+		adapter: adapter({
+			pages: 'build',
+			assets: 'build',
+			fallback: undefined,
+			precompress: false,
+			strict: true
+		}),
+		paths: {
+			base: process.env.NODE_ENV === 'production' ? '/big-trouble' : ''
+		}
+	},
 	vitePlugin: {
 		dynamicCompileOptions: ({ filename }) =>
 			filename.includes('node_modules') ? undefined : { runes: true }
