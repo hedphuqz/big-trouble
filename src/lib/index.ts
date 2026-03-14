71394871f3c7b0f3423010be5f0dc7f542c2ae0e
Francis Powlesland <francis.powlesland1@ibm.com>
2026-03-14 06:09:40 +0100

    feat: add core application structure
    - Add app.d.ts for TypeScript type definitions
    - Add app.html as the main HTML template
    - Add lib/index.ts for library exports

---
+++ src/lib/index.ts
@@ -0,0 +1,3 @@
+// place files you want to import through the `$lib` alias in this folder.
+export { default as List } from './components/List.svelte';
+export { default as TextInput } from './components/TextInput.svelte';
